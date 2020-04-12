706965fb85bca95fb282732c2c58dc8c7fd6cd12
Showmen Talukder <38407270+SoulSen@users.noreply.github.com>
2020-04-12 13:43:05 -0700

    Update Notifier.py

--- tests/Notifier.py
+++ tests/Notifier.py
@@ -6,8 +6,7 @@ from hangups import CredentialsPrompt, RefreshTokenCache
 from hanger import Client
 
 asyncio.run(Client(
-    hangups.get_auth(
-        CredentialsPrompt(),
-        RefreshTokenCache('refresh-token.txt'),
+    hangups.get_auth_stdin(
+        RefreshTokenCache('./refresh-token.txt'),
         manual_login=True)
 )._hangups_client.connect())
