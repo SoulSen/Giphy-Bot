c6140a1c73ead4ddc8450d5a625491af1164a4f3
Showmen Talukder <38407270+SoulSen@users.noreply.github.com>
2020-04-12 17:48:39 -0700

    Update client.py

--- hanger/client.py
+++ hanger/client.py
@@ -100,6 +100,7 @@ class Client:
             if event.text[1:].lower().startswith('giphy') or event.text[1:].lower().startswith('stickers'):
                 try:
                     query = " ".join(event.text.split(' ')[1:])
+                    print(event.text.split(' ')[1:])
                     if query == " ":
                         raise IndexError
                 except IndexError:
